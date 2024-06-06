29c89c02e20a8ab6a7bc4c5c0968174d353a8ec1
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-06-06 17:58:50 +0200

    change search results to present helpful info

--- entry/search.ts
+++ entry/search.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import MeiliSearch from 'meilisearch';
 import minimist from 'minimist';
 
+import { Post } from '../lib/types';
+
 async function main() {
 	const args = minimist(process.argv.slice(2));
 	const client = new MeiliSearch({
@@ -10,14 +12,41 @@ async function main() {
 	});
 
 	if (args._.length === 0) {
-		console.error('Usage: search <query>');
+		console.error('Usage: search [--page N] [--no-facets] <query>');
 		process.exit(1);
 	}
 
 	const query = args._.join(' ');
-	console.log('Searching for:', query);
-	const response = await client.index('tumblr').search(query);
-	console.log(response);
+	const page = args.page ? parseInt(args.page, 10) : 1;
+	const noFacets = args.facets === false;
+	const response = await client
+		.index('tumblr')
+		.search<Post>(query, { facets: ['tags', 'type'], page });
+	const hitCount =
+		response.totalHits ??
+		`~${response.estimatedTotalHits}` ??
+		response.hits.length;
+	console.log(
+		`Got ${hitCount} results for ${query} in ${response.processingTimeMs}ms:`,
+	);
+	const indexOffset = (page - 1) * response.hitsPerPage;
+	console.log(
+		response.hits
+			.map(
+				(hit, index) => `\t${index + 1 + indexOffset}: ${hit.post_url}`,
+			)
+			.join('\n'),
+	);
+	if (noFacets) {
+		return;
+	}
+	console.log();
+	for (const [facet, values] of Object.entries(response.facetDistribution)) {
+		console.log(`${facet}:`);
+		for (const [value, count] of Object.entries(values)) {
+			console.log(`\t${value}: ${count}`);
+		}
+	}
 }
 
 main();
